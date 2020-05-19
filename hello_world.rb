c10d366195f7f52423069e077e862f4b6f8fc5e8
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-19 16:05:38 +0200

    display information for each person new update

--- hello_world.rb
+++ hello_world.rb
@@ -1,36 +1,4 @@
-
-names = ['Felix', 'Olivia', 'Twinky']
+names = ['Thomas', 'Noel', 'Faraz']
 names.each do |name|
-age = 28, 29, 1 
-gender = "male", "female", "female dog"
-puts "Hello #{name} is a #{age} year old #{gender}"
-end
-
-
-my_group = Array.new
-my_group = 
-
-[person_1 = {name: "Felix", gender: "male", age: 28}]
-
-[person_2 = {name: "Olivia", gender: "female", age: 29}]
-
-[person_3 = {name: "Twinky", gender: "female dog", age: 1}]
-
-names = ["Felix"]
-names.each do |name|
-age = 28
-gender = "male"
-puts "#{name} is a #{age} year old #{gender}"
-end
-names = ["Olivia"]
-names.each do |name|
-age = 29
-gender = "female"
-puts "#{name} is a #{age} year old #{gender}"
-end
-names = ["Twinky"]
-names.each do |name|
-age = 1
-gender = "female dog"
-puts "#{name} is a #{age} year old #{gender}"
+puts "Hello #{name}"
 end
