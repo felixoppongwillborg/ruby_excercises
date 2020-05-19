80e78c275dc5d127b2046b8ff18cbfe2defab212
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-19 17:22:11 +0200

    corrected information for each person

--- hello_world.rb
+++ hello_world.rb
@@ -1,4 +1,29 @@
-names = ['Thomas', 'Noel', 'Faraz']
-names.each do |name|
-puts "Hello #{name}"
+my_group = [
+
+
+person_1 ={ 
+            "name" =>'Felix',
+            "gender" =>'male',
+            "age" => 28
+},
+
+
+person_2 ={ 
+            "name" => "Olivia",
+             "gender" => "female",
+              "age" => 29 
+},
+
+
+person_3 ={
+            "name" => "Twinky",
+            "gender" => "female dog",
+            "age" => 1
+
+}
+
+]
+
+my_group.each do |person|
+    puts "#{person["name"]} is a #{person["age"]} year old #{person["gender"]}"
 end
--- my_group.rb
+++ my_group.rb
@@ -1,27 +1,4 @@
-my_group = Array.new
-my_group = 
-
-[person_1 = {name: "Felix", gender: "male", age: 28}]
-
-[person_2 = {name: "Olivia", gender: "female", age: 29}]
-
-[person_3 = {name: "Twinky", gender: "female dog", age: 1}]
-
-names = ["Felix"]
+names = ['Thomas', 'Noel', 'Faraz']
 names.each do |name|
-age = 28
-gender = "male"
-puts "#{name} is a #{age} year old #{gender}"
-end
-names = ["Olivia"]
-names.each do |name|
-age = 29
-gender = "female"
-puts "#{name} is a #{age} year old #{gender}"
-end
-names = ["Twinky"]
-names.each do |name|
-age = 1
-gender = "female dog"
-puts "#{name} is a #{age} year old #{gender}"
+puts "Hello #{name}"
 end
