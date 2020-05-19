8bcb269a0adbb3a18acc6ed3cfead827d8a23851
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-19 17:24:16 +0200

    corrected information for eachperson in my_group

--- hello_world.rb
+++ hello_world.rb
@@ -1,29 +1,4 @@
-my_group = [
-
-
-person_1 ={ 
-            "name" =>'Felix',
-            "gender" =>'male',
-            "age" => 28
-},
-
-
-person_2 ={ 
-            "name" => "Olivia",
-             "gender" => "female",
-              "age" => 29 
-},
-
-
-person_3 ={
-            "name" => "Twinky",
-            "gender" => "female dog",
-            "age" => 1
-
-}
-
-]
-
-my_group.each do |person|
-    puts "#{person["name"]} is a #{person["age"]} year old #{person["gender"]}"
+names = ['Thomas', 'Noel', 'Faraz']
+names.each do |name|
+puts "Hello #{name}"
 end
