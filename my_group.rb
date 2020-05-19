b6edf4f4f05ea2df43f347cc75fdc9bd1bdd5431
felixoppongwillborg <felixwillborg@MacBook-Pro-som-tillhor-Felix.local>
2020-05-19 11:38:41 +0200

    added 3 persons in array and created hashes

--- my_group.rb
+++ my_group.rb
@@ -1,1 +1,11 @@
-my_group = Array.new
+my_group = [
+
+person_1 = {name: "Felix", gender: "male", age: 28}
+
+person_2 = {name: "Olivia", gender: "female", age: 29}
+
+person_3 = {name: "Twinky", gender: "female dog", age: 1}
+
+
+
+]
